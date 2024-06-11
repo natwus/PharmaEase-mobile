2b857271c25c835c43b79dab0821b3517621e841
luiz <mareussenai@gmail.com>
2024-06-11 17:07:39 -0300

    parte luiz

--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -5,7 +5,6 @@ import { Botao } from "../componentes/Botao";
 import { EntradaTexto } from "../componentes/EntradaTexto";
 import { Titulo } from "../componentes/titulo";
 import { depoimentos } from "../utils/mock";
-<<<<<<< HEAD
 import { buscarEspecialistaPorEstado } from "../servicos/EspecialistaServico";
 import { CardConsulta } from "../componentes/CardConsulta";
 import Dipirona from '../assets/dipirona.png';
@@ -15,12 +14,6 @@ import Amoxilina from '../assets/Amoxilina.png'
 import Ciprofloxacino from '../assets/ciprofloxacino.png'
 import Cefalexina from '../assets/Cefalexina.png'
 import { Remedios } from '../remedios/Remedios';
-=======
-import { CardConsulta } from "../componentes/CardConsulta";
-import { buscarEspecialistaPorEstado } from "../servicos/EspecialistaServico";
-import { useState } from "react";
-
->>>>>>> 245b7cca84095f7efda197c720bb147615837202
 
 interface Especialista {
   nome: string,
@@ -29,7 +22,6 @@ interface Especialista {
   id: string,
 }
 
-<<<<<<< HEAD
 const Analgezico = [
   {
     id: 1,
@@ -74,9 +66,6 @@ const Antibióticos = [
 ];
 
 export default function Principal({ navigation }) {
-=======
-export default function Principal({navigation}){
->>>>>>> 245b7cca84095f7efda197c720bb147615837202
   const [estado, setEstado] = useState('');
   const [especialidade, setEspecialidade] = useState('');
   const [resultadoBusca, setResultadoBuscar] = useState([]);
@@ -90,21 +79,13 @@ export default function Principal({navigation}){
     }
   }
 
-  function voltarSecao() {
-    navigation.replace('Login')
-   }
-
   return (
     <ScrollView flex={1} bgColor="white">
       <VStack flex={1} alignItems="flex-start" justifyContent="flex-start" p={5}>
-<<<<<<< HEAD
         <Image source={Logo} alt="Logo" mt={10} />
         <Titulo color="blue.500">Boas-vindas!</Titulo>
 
         <Box w="100%" borderRadius="lg" p={3} mt={10} shadow="1" borderRightRadius="md">
-=======
-        <Box w="100%" borderRadius="lg" p={3} mt={5} shadow="1" borderRightRadius="md">
->>>>>>> 245b7cca84095f7efda197c720bb147615837202
           <EntradaTexto
             placeholder="Digite a especialidade"
             value={especialidade}
@@ -119,10 +100,6 @@ export default function Principal({navigation}){
             Buscar
           </Botao>
         </Box>
-
-
-        <Titulo color="red.800" alignSelf="center">Depoimentos</Titulo>
-        <VStack space={3} divider={<Divider />} w="100%">
         {resultadoBusca?.map((especialista: Especialista, index) => (
           <VStack flex={1} w="100%" alignItems="flex-start" bgColor="white" key={index}>
             <CardConsulta
@@ -130,12 +107,9 @@ export default function Principal({navigation}){
               foto={especialista.imagem}
               nome={especialista.nome}
               onPress={() => navigation.navigate('Agendamento', { especialistaId: especialista.id })}
-              
             />
-             
           </VStack>
         ))}
-<<<<<<< HEAD
 
         <Titulo color="blue.800" alignSelf="center">Remédios</Titulo>
         <Box w="100%" borderRadius="lg" p={3} shadow={1} mt={5}>
@@ -148,15 +122,6 @@ export default function Principal({navigation}){
                   description={remedio.description}
                   // onPress={() => navigation.navigate('Agendamento', { remedioId: remedio.id })}
                 />
-=======
-          {
-            depoimentos.map(depoimento => (
-              <Box key={depoimento.id} w="100%" borderRadius="lg" p={3}>
-                <Text color="gray.300" fontSize="md" textAlign="justify">
-                  {depoimento.text}
-                </Text>
-                <Text color="gray.500" fontSize="lg" fontWeight="bold" alignSelf="center" mt="2">{depoimento.titulo}</Text>
->>>>>>> 245b7cca84095f7efda197c720bb147615837202
               </Box>
             ))}
           </ScrollView>
