98bbad3d949526b1a37534c0455519f70d682743
Julia <mareussenai@gmail.com>
2024-06-12 17:13:38 +0044

    Perfil
    estilizei o perfil e a nav bar

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,5 +1,5 @@
-import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base'
-import { Titulo } from '../componentes/titulo'
+import { VStack, Text, ScrollView, Avatar, Divider, Box } from 'native-base'
+import { Titulo } from '../componentes/Titulo'
 import { pegarDadosPaciente } from '../servicos/PacienteServico'
 import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -30,19 +30,19 @@ export default function Perfil({ navigation }) {
 
   return (
     <ScrollView flex={1}>
-      <VStack flex={1} alignItems="center" p={5}>
-        <Titulo color="red.500">Meu Perfil</Titulo>
+      <VStack flex={1} alignItems="center">
+          <Box backgroundColor="#1C1C1C" m="auto" alignItems="center" w="100%" paddingBottom="5">
+            <Titulo color="red.500">Meu Perfil</Titulo>
+            <Avatar size="175" borderColor="#fff" borderWidth="5" source={{ uri: dadosPaciente?.imagem }} mt={5} />
+            <Titulo fontSize="xl" mb={1} color="#fff">{dadosPaciente.nome}</Titulo>
+        </Box>
 
-        <Avatar size="xl" source={{ uri: dadosPaciente?.imagem }} mt={5} />
-
-        <Titulo color="red.500">Informações pessoais</Titulo>
+        <Titulo color="red.500" mb={1}>Suas Informações</Titulo>
+        
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
-        <Text>Email: {dadosPaciente?.email}</Text>
-        <Text>Estado: {dadosPaciente?.endereco?.estado}</Text>
-        <Text>Telefone: {dadosPaciente?.telefone}</Text>
-
-
-        <Divider mt={5} />
+        <Text fontSize="md" borderColor="#1C1C1C" borderWidth="1" borderRadius="20" p="2" m="2">Email:            {dadosPaciente?.email}</Text>
+        <Text fontSize="md" borderColor="#1C1C1C" borderWidth="1" borderRadius="20" p="2" m="2">Estado:                           {dadosPaciente?.endereco?.estado}</Text>
+        <Text fontSize="md" borderColor="#1C1C1C" borderWidth="1" borderRadius="20" p="2" m="2">Telefone:                {dadosPaciente?.telefone}</Text>
 
         <Titulo color="red.500" mb={1}>Planos de Saúde</Titulo>
         {
@@ -50,7 +50,8 @@ export default function Perfil({ navigation }) {
             <Text key={index}>{plano}</Text>
           ))
         }
-        <Botao backgroundColor="red.600" onPress={deslogar}>
+
+        <Botao backgroundColor="red.500" borderRadius="5" w="400" onPress={deslogar}>
           Deslogar
         </Botao>
       </VStack>
--- src/estilos/temas.ts
+++ src/estilos/temas.ts
@@ -6,8 +6,8 @@ export const TEMAS = extendTheme({
       300: '#8D8D99'
     },
     red: {
-      500: '#EB3636',
-      800: '#EB0000'
+      500: '#DF0101',
+      800: '#E452'
     },
     white: '#eee',
     black: '#333'
