9f6cd3f12a8901ef9d1c8637dea56c3976e66efc
natwus <mareussenai@gmail.com>
2024-06-15 15:29:44 -0300

    v1.1.2
    anteriormente estava abrindo o app de email para enviar a mensagem, agora, envia pelo proprio app

--- src/Tabs/Contato.tsx
+++ src/Tabs/Contato.tsx
@@ -3,7 +3,6 @@ import { VStack, ScrollView, Center, useToast } from 'native-base';
 import { Titulo } from '../componentes/titulo';
 import { EntradaTexto } from '../componentes/EntradaTexto';
 import { Botao } from '../componentes/Botao';
-import * as MailComposer from 'expo-mail-composer';
 
 export default function Contato() {
   const [nome, setNome] = useState('');
@@ -12,11 +11,6 @@ export default function Contato() {
   const toast = useToast();
 
   const EnviarMensagem = async () => {
-    await MailComposer.composeAsync({
-      recipients: [email],
-      subject: nome,
-      body: mensagem
-    })
     if (!nome || !email || !mensagem) {
       toast.show({
         title: "Erro",
@@ -25,56 +19,56 @@ export default function Contato() {
         duration: 3000
       });
       return;
-    } else {
-
-      toast.show({
-        title: "Mensagem enviada",
-        description: "Obrigado por entrar em contato!",
-        backgroundColor: "green.500",
-        duration: 3000
-      });
     }
 
-    setNome('');
-    setEmail('');
-    setMensagem('');
+    try {
+      const response = await fetch('http://192.168.1.107:4000/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          to: 'biotech.pharmaease@gmail.com',
+          subject: `Mensagem de ${nome}, ${email}`,
+          text: mensagem,
+        }),
+      });
 
+      const data = await response.json();
+
+      if (response.ok) {
+        setNome('');
+        setEmail('');
+        setMensagem('');
+        toast.show({
+          title: "Sucesso",
+          description: "Mensagem enviada com êxito!",
+          backgroundColor: "green.500",
+          duration: 3000
+        });
+      } else {
+        throw new Error(data.error || 'Erro ao enviar o e-mail');
+      }
+    } catch (error) {
+      toast.show({
+        title: "Erro",
+        description: error.message || "Falha ao tentar enviar a mensagem.",
+        backgroundColor: "red.500",
+        duration: 3000
+      });
+      console.log(error)
+    }
   };
 
   return (
     <ScrollView flex={1} bgColor="#f0f0f0">
       <Center flex={1} py={10}>
         <VStack w="90%" borderRadius="xl" p={8} shadow="5" bgColor="white" space={4}>
-          <Titulo fontSize="2xl" color="red.500" mb={5}>
-            CONTATO
-          </Titulo>
-          <EntradaTexto
-            placeholder="Nome"
-            value={nome}
-            onChangeText={setNome}
-          />
-          <EntradaTexto
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <EntradaTexto
-            placeholder="Mensagem"
-            value={mensagem}
-            onChangeText={setMensagem}
-          />
-          <Botao
-            onPress={EnviarMensagem}
-            backgroundColor="red.500"
-            _text={{ color: "white" }}
-            borderRadius="md"
-            _hover={{ backgroundColor: "red.600" }}
-            _pressed={{ backgroundColor: "red.700" }}
-            shadow="3"
-            mb={2}
-          >
-            Enviar Mensagem
-          </Botao>
+          <Titulo fontSize="2xl" color="red.500" mb={5}>CONTATO</Titulo>
+          <EntradaTexto placeholder="Nome" value={nome} onChangeText={setNome} />
+          <EntradaTexto placeholder="Email" value={email} onChangeText={setEmail} />
+          <EntradaTexto placeholder="Mensagem" value={mensagem} onChangeText={setMensagem} />
+          <Botao onPress={EnviarMensagem} backgroundColor="red.500" _text={{ color: "white" }} borderRadius="md" _hover={{ backgroundColor: "red.600" }} _pressed={{ backgroundColor: "red.700" }} shadow="3" mb={2}>Enviar Mensagem</Botao>
         </VStack>
       </Center>
     </ScrollView>
