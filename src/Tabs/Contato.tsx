bd4624e42af486fa82b3070b15f9a2b1f5cbcc66
mareus <mareussenai@gmail.com>
2024-06-17 11:19:21 +0044

    Update Contato.tsx

--- src/Tabs/Contato.tsx
+++ src/Tabs/Contato.tsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { VStack, ScrollView, Center, useToast } from 'native-base';
 import { Titulo } from '../componentes/titulo';
 import { EntradaTexto } from '../componentes/EntradaTexto';
 import { Botao } from '../componentes/Botao';
+import { Paciente } from '../interfaces/Paciente';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { pegarDadosPaciente } from '../servicos/PacienteServico';
 
 export default function Contato() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [mensagem, setMensagem] = useState('');
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente);
   const toast = useToast();
+
+  useEffect(() => {
+    async function dadosPaciente() {
+      const pacienteId = await AsyncStorage.getItem('pacienteId');
+      if (!pacienteId) return null;
+
+      const resultado = await pegarDadosPaciente(pacienteId);
+      if (resultado) {
+        setDadosPaciente(resultado);
+      }
+    }
+    dadosPaciente();
+  })
 
   const EnviarMensagem = async () => {
     // verificar campo vazio
@@ -68,7 +85,7 @@ export default function Contato() {
         <VStack w="90%" borderRadius="xl" p={8} shadow="5" bgColor="white" space={4}>
           <Titulo fontSize="2xl" color="red.500" mb={5}>CONTATO</Titulo>
           <EntradaTexto placeholder="Nome" value={nome} onChangeText={setNome} />
-          <EntradaTexto placeholder="Email" value={email} onChangeText={setEmail} />
+          <EntradaTexto placeholder="Email" value={dadosPaciente.email} onChangeText={setEmail} />
           <EntradaTexto placeholder="Mensagem" value={mensagem} onChangeText={setMensagem} />
           <Botao onPress={EnviarMensagem} backgroundColor="red.500" _text={{ color: "white" }} borderRadius="md" _hover={{ backgroundColor: "red.600" }} _pressed={{ backgroundColor: "red.700" }} shadow="3" mb={2}>Enviar Mensagem</Botao>
         </VStack>
